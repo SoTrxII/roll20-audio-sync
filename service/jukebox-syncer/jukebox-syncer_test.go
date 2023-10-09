3ba6f6bd1b9b6762fddcd866cfcdcb90888b7098
SoTrxII <lo.lo.@lo.lo>
2023-10-09 17:47:51 +0200

    test(jukebox-syncer): Adding a test case for state retention

--- service/jukebox-syncer/jukebox-syncer_test.go
+++ service/jukebox-syncer/jukebox-syncer_test.go
@@ -11,6 +11,42 @@ func TestJukeboxSyncer_HandleStateIsNil(t *testing.T) {
 	err := s.Handle(nil)
 	assert.Error(t, err)
 }
+
+// Assert they're no state retention when a recording stops
+func TestJukeboxSyncer_StateDeletion(t *testing.T) {
+	s := NewJukeboxSyncer(&mockMixer{})
+	err := s.Start("1")
+	assert.NoError(t, err)
+	err = s.Handle(&R20State{
+		Rid: "1",
+		Uid: "2",
+		Tracks: []R20Track{
+			{
+				Url:     "a",
+				Playing: true,
+			},
+		},
+	})
+	assert.NoError(t, err)
+	err = s.Stop("1")
+	assert.NoError(t, err)
+	err = s.Start("1")
+	assert.NoError(t, err)
+	// If state has been kept, this will throw an error
+	// as the user ID is different
+	err = s.Handle(&R20State{
+		Rid: "1",
+		Uid: "3",
+		Tracks: []R20Track{
+			{
+				Url:     "a",
+				Playing: true,
+			},
+		},
+	})
+	assert.NoError(t, err)
+
+}
 func TestJukeboxSyncer_HandleStateBeforeStartError(t *testing.T) {
 	s := NewJukeboxSyncer(&mockMixer{})
 	err := s.Handle(&R20State{
